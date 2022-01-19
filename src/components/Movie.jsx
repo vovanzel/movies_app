8affe2ef34d67e29ea24aff8f405118dd1402742
vovanzel <kolbvova@gmail.com>
2022-01-19 14:26:40 +0300

    Some changes

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -11,9 +11,9 @@ function Movie (props) {
         <div className="card-image waves-effect waves-block waves-light">
             {
                 poster === 'N/A' ?
-                    <img className="activator" src={`https://via.placeholder.com/300x450.png?text=${title}`} />
+                    <img className="activator" src={`https://via.placeholder.com/300x450.png?text=${title}`} alt={`${title}`}/>
                     :
-                    <img className="activator" src={poster} />
+                    <img className="activator" src={poster} alt={`${title}`}/>
             }
 
         </div>
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return <nav className="green darken-1">
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">React Movies</a>
+            <a href="!#" className="brand-logo">React Movies</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="!#">Repo</a></li>
             </ul>
